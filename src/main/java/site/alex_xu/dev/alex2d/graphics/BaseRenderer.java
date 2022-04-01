4a840d45aac18ada321510755b6e0cefb1823fef
alex <alex.zihao.xu@gmail.com>
2022-04-01 11:54:47 -0400

    Added: Renderer

--- src/main/java/site/alex_xu/dev/alex2d/graphics/BaseRenderer.java
+++ src/main/java/site/alex_xu/dev/alex2d/graphics/BaseRenderer.java
@@ -1,64 +1,82 @@
 package site.alex_xu.dev.alex2d.graphics;
 
+import org.joml.Matrix4f;
 import site.alex_xu.dev.alex2d.graphics.abstracting.AbstractFrameI;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 abstract class BaseRenderer {
 
     // Stroke
 
-    void strokeLine(float x1, float y1, float x2, float y2) {
+    public void strokeLine(float x1, float y1, float x2, float y2) {
         // TODO
     }
 
-    void strokeTriangle(float x1, float y1, float x2, float y2, float x3, float y3) {
+    public void strokeTriangle(float x1, float y1, float x2, float y2, float x3, float y3) {
         // TODO
     }
 
-    void strokeCircle(float x, float y, float radius) {
+    public void strokeCircle(float x, float y, float radius) {
         // TODO
     }
 
-    void strokeRect(float x, float y, float w, float h) {
+    public void strokeRect(float x, float y, float w, float h) {
         // TODO
     }
 
     // Fill
 
-    void fillTriangle(float x1, float y1, float x2, float y2, float x3, float y3) {
+    public void fillTriangle(float x1, float y1, float x2, float y2, float x3, float y3) {
         // TODO
     }
 
-    void fillCircle(float x, float y, float radius) {
+    public void fillCircle(float x, float y, float radius) {
         // TODO
     }
 
-    void fillRect(float x, float y, float w, float h) {
+    public void fillRect(float x, float y, float w, float h) {
         // TODO
     }
 
     // Image
 
-    void drawImage(AbstractFrameI image, float srcX, float srcY, float srcW, float srcH, float dstX, float dstY, float dstW, float dstH) {
+    public void drawImage(AbstractFrameI image, float srcX, float srcY, float srcW, float srcH, float dstX, float dstY, float dstW, float dstH) {
         // TODO
     }
 
     // Transformations
 
-    void translate(float x, float y) {
+    public void translate(float x, float y) {
         // TODO
     }
 
-    void rotate(float r) {
+    public void rotate(float r) {
         // TODO
     }
 
-    void scale(float x, float y) {
+    public void scale(float x, float y) {
         // TODO
+    }
+
+    public void pushMatrix() {
+        // TODO
+    }
+
+    public void popMatrix() {
+        // TODO
+    }
+
+    public void resetMatrix() {
+        // TODO
+    }
+
+    public Matrix4f copyMatrix() {
+        throw new NotImplementedException();
     }
 
     // Setters
 
-    void setColor(float r, float g, float b, float a) {
+    public void setColor(float r, float g, float b, float a) {
         // TODO
     }
 
---
+++ src/main/java/site/alex_xu/dev/alex2d/graphics/Renderer.java
@@ -0,0 +1,106 @@
+package site.alex_xu.dev.alex2d.graphics;
+
+import org.joml.Vector2f;
+import site.alex_xu.dev.alex2d.graphics.abstracting.AbstractFrameI;
+
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+
+public class Renderer extends BaseRenderer {
+
+    // Stroke
+
+    public void strokeLine(Vector2f p1, Vector2f p2) {
+        strokeLine(p1.x, p1.y, p2.x, p2.y);
+    }
+
+    public void strokeTriangle(Vector2f p1, Vector2f p2, Vector2f p3) {
+        strokeTriangle(p1.x, p1.y, p2.x, p2.y, p3.x, p3.y);
+    }
+
+    public void strokeCircle(Vector2f center, float radius) {
+        strokeCircle(center.x, center.y, radius);
+    }
+
+    public void strokeRect(Rectangle rect) {
+        strokeRect(rect.x, rect.y, rect.width, rect.height);
+    }
+
+    public void strokeRect(Rectangle2D rect) {
+        strokeRect((float) rect.getX(), (float) rect.getY(), (float) rect.getWidth(), (float) rect.getHeight());
+    }
+
+    public void strokeRect(Rectangle2D.Float rect) {
+        strokeRect(rect.x, rect.y, rect.width, rect.height);
+    }
+
+    public void strokeRect(Vector2f pos, float width, float height) {
+        strokeRect(pos.x, pos.y, width, height);
+    }
+
+    // Fill
+
+    public void fillTriangle(Vector2f p1, Vector2f p2, Vector2f p3) {
+        fillTriangle(p1.x, p1.y, p2.x, p2.y, p3.x, p3.y);
+    }
+
+    public void fillCircle(Vector2f center, float radius) {
+        fillCircle(center.x, center.y, radius);
+    }
+
+    public void fillRect(Rectangle rect) {
+        fillRect(rect.x, rect.y, rect.width, rect.height);
+    }
+
+    public void fillRect(Rectangle2D rect) {
+        fillRect((float) rect.getX(), (float) rect.getY(), (float) rect.getWidth(), (float) rect.getHeight());
+    }
+
+    public void fillRect(Rectangle2D.Float rect) {
+        fillRect(rect.x, rect.y, rect.width, rect.height);
+    }
+
+    public void fillRect(Vector2f pos, float width, float height) {
+        fillRect(pos.x, pos.y, width, height);
+    }
+
+    // Image
+
+    public void drawImage(AbstractFrameI image, float srcX, float srcY, float srcW, float srcH, float dstX, float dstY) {
+        drawImage(image, srcX, srcY, srcW, srcH, dstX, dstY, srcW, srcH);
+    }
+
+    public void drawImage(AbstractFrameI image, float x, float y) {
+        drawImage(image, 0, 0, image.getWidth(), image.getHeight(), x, y, image.getWidth(), image.getHeight());
+    }
+
+    // Transformations
+
+    public void translate(Vector2f xy) {
+        translate(xy.x, xy.y);
+    }
+
+    public void scale(Vector2f xy) {
+        scale(xy.x, xy.y);
+    }
+
+    public void scale(float scale) {
+        scale(scale, scale);
+    }
+
+    // Setters
+
+    public void setColor(float r, float g, float b) {
+        setColor(r, g, b, 1);
+    }
+
+    public void setColor(float brightness, float alpha) {
+        setColor(brightness, brightness, brightness, alpha);
+    }
+
+    public void setColor(float brightness) {
+        setColor(brightness, 1);
+    }
+
+
+}
